fcbe48bc864c65c02646dff367564499b8c7c9b0
Tilde Bastos <tilde.bastos@gmail.com>
2022-03-02 00:06:38 -0300

    update <3

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace _07_ByteBank {
+    public class ContaCorrente {
+
+        // titular é um tipo de referência a classe Cliente
+        public Cliente Titular { get; set; }
+
+        private int _agencia;
+        public int Agencia {
+
+            get {
+                return _agencia;
+            }
+            set {
+                if (value <= 0) {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+
+        public int Numero { get; set; }
+        private double _saldo = 100;
+
+        // método construtor da conta corrente
+        public ContaCorrente(int agencia, int numero) { 
+            
+            Agencia = agencia;
+            Numero = numero;
+        
+        }
+
+        public double Saldo {
+            get {
+                return _saldo;
+            }
+            set {
+                if (value < 0) {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        // this para acessar um atributo
+        public bool Sacar(double valor) {
+
+            if (_saldo < valor) {
+                return false;
+            }
+            else {
+                _saldo -= valor;
+                return true;
+            }
+        }
+        // quando nosso método não retorna nenhum valor -> "void"
+        public void Depositar(double valor) {
+
+            _saldo += valor;
+        }
+        public bool Transferir(double valor, ContaCorrente contaDestino) {
+            if (_saldo < valor) {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+
+
+        }
+    }
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07_ByteBank {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            ContaCorrente conta = new ContaCorrente(123, 111222334);
+
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(conta.Agencia);
+
+            Console.ReadLine();
+
+        }
+    }
+}

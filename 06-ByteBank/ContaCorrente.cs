69e1bf2fdfbc9c5c006b4135b2a6a554ab81456c
Tilde Bastos <tilde.bastos@gmail.com>
2022-03-01 23:18:02 -0300

    update ^_^

--- 06-ByteBank/ContaCorrente.cs
+++ 06-ByteBank/ContaCorrente.cs
@@ -3,40 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _05_ByteBank;
 
 
-namespace _05_ByteBank {
+
+namespace _06_ByteBank {
     public class ContaCorrente {
 
         // titular é um tipo de referência a classe Cliente
-        public Cliente titular;
-        public int agencia;
-        public int numero;
-        public double saldo = 100;
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        private double _saldo = 100;
+
+        public double Saldo {
+            get {
+                return _saldo;
+            }
+            set {
+                if (value < 0) {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
 
         // this para acessar um atributo
         public bool Sacar(double valor) {
 
-            if (this.saldo < valor) {
+            if (_saldo < valor) {
                 return false;
             }
             else {
-                this.saldo -= valor;
+                _saldo -= valor;
                 return true;
             }
         }
         // quando nosso método não retorna nenhum valor -> "void"
         public void Depositar(double valor) {
 
-            this.saldo += valor;
+            _saldo += valor;
         }
         public bool Transferir(double valor, ContaCorrente contaDestino) {
-            if (this.saldo < valor) {
+            if (_saldo < valor) {
                 return false;
             }
 
-            this.saldo -= valor;
+            _saldo -= valor;
             contaDestino.Depositar(valor);
             return true;
 
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06_ByteBank {
+    internal class Program {
+        static void Main(string[] args) {
+
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Maria";
+            cliente.CPF = "123.000.000-00";
+            cliente.Profissao = "Desenvolvedora C#";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
